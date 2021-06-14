0d9718c1501cfb08c3e80f910e72b2b6b9673081
paul-carlton <paul.carlton414@gmail.com>
2021-06-14 10:25:46 +0100

    add aws auth to install

--- scripts/install-utils.sh
+++ scripts/install-utils.sh
@@ -43,6 +43,10 @@ curl -s -Lo ./kind https://kind.sigs.k8s.io/dl/v0.10.0/kind-linux-amd64 && \
 chmod +x ./kind && \
 sudo mv ./kind /usr/local/bin/
 
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+sudo mv ./aws-iam-authenticator /usr/local/bin
+
 curl -s -LO "https://dl.k8s.io/release/v1.18.8/bin/linux/amd64/kubectl" && \
 chmod +x ./kubectl && \
 sudo mv ./kubectl /usr/local/bin
